7151ffd63d1b2dbe363e77da39a9a5fb11638de8
Amin Meshk <amin.meshk@gmail.com>
2022-09-03 16:47:35 +0430

    Add Article Model

---
+++ database/factories/ArticleFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Article>
+ */
+class ArticleFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'user_id' => fake()->numberBetween(1, 5),
+            'title' => fake()->sentence(4),
+            'content' => fake()->paragraphs(8, true)
+        ];
+    }
+}
